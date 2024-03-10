1ed1998ea48d607b267298c1bda8ce71bc8e9323
iidogpon <160293233+Aidogpon@users.noreply.github.com>
2024-03-10 08:47:08 +0700

    add import fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,70 @@
-import '@/app/ui/global.css';
-import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+
 import Link from 'next/link';
-import { lusitana } from './ui/fonts';
+import Image from 'next/image';
+import { lusitana } from '@/app/ui/fonts';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p
-            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
-          >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
+    <main>
+      <div className="flex h-screen w-full flex-col items-center justify-center xl:flex-row xl:gap-3">
+        <div className="flex h-full w-full flex-1 justify-center rounded-lg pb-6  lg:items-center xl:px-6">
+          <div className="flex h-full w-[85%] flex-col justify-center gap-6 lg:h-full 2xl:w-[70%]">
+            <div className="w-full items-center justify-center sm:flex  md:px-28 xl:hidden xl:p-6 xl:py-12">
+              <Image
+                src="/background-landing-page.png"
+                width={800}
+                height={760}
+                className="xl:hidden"
+                alt="Screenshots of the dashboard project showing desktop version"
+              />
+            </div>
+            <div className="row-span-2">
+              <strong
+                className={`${lusitana.className} mt-[-20px] text-[16px] xl:text-[40px]`}
+              >
+                <p className="text-center">
+                  Welcome to finance dashboard project !!
+                </p>
+              </strong>
+
+              <span className="text-[13px]  xl:text-[18px]">
+                <p className="text-center">
+                  สวัสดีครับ, ขอบคุณที่เข้ามาเยี่ยมชม
+                </p>
+              </span>
+              <div className="mt-5 flex w-full justify-center xl:mt-10">
+                <Link
+                  href="/login"
+                  className="mt-3 flex w-[150px] items-center gap-3 rounded-lg bg-blue-500 px-6 py-2 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+                >
+                  <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+                </Link>
+              </div>
+
+              <div className="mt-5 flex w-full justify-center">
+                <p className="text-center text-[10px] text-gray-600 xl:text-[11px]">
+                  You can see the instructions for access at the{' '}
+                  <Link
+                    href="https://github.com/Aidogpon/nextjs-financial-dashboard"
+                    className="text-blue-400"
+                  >
+                    Github-repository
+                  </Link>{' '}
+                  to log in.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+        <div className="hidden items-center justify-center md:w-3/5 md:px-20 md:py-12 lg:p-6 xl:flex">
+          <Image
+            src="/background-landing-page.png"
+            width={800}
+            height={760}
+            className="hidden lg:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
         </div>
       </div>
     </main>
